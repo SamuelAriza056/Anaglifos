e6698009ff0991feb87e915d7fd3ff97a6886aec
ph3lb0t <137623605+mote44@users.noreply.github.com>
2024-10-04 17:08:08 +0200

    a

---
+++ Anaglifos/Assets/Anaglifos_Root/Scripts/Audio_Manager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Manager : MonoBehaviour
+{
+    [Header("_________Audio Source__________")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("_________Clips_________")]
+    public AudioClip background;
+    public AudioClip Death;
+    public AudioClip Color;
+    public AudioClip LevelStart;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+        PlaySFX(LevelStart);
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
